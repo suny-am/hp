a66126dceca263ddc5f222baaa74911a309d64b9
suny-am <visualarea.1@gmail.com>
2024-02-26 22:37:39 +0100

    base templating started

---
+++ src/components/Landing.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div class="logo">
+        <h2>CarlSandberg</h2>
+        <h1>DesignWorks</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>gallery</li>
+          <li>blog</li>
+          <li>shop</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>side section here</section>
+      <section>side section 2 here</section>
+      <section>side section 3 here</section>
+      <section>side section 4 here</section>
+    </main>
+  </div>
+</template>
