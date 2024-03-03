f233b84bed125622397f2a76ae8f07d491fe87e7
suny-am <visualarea.1@gmail.com>
2024-03-03 14:46:30 +0100

    reverting to dist outdir

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: "./docs"
-  },
+  base: "/hp/",
   server: {
     port: 3000,
     watch: {
