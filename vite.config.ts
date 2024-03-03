40c0c1ae3da8261bde88ca6eb0fe427a99888c15
suny-am <visualarea.1@gmail.com>
2024-03-03 14:33:34 +0100

    updated outDir for vite builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "./docs"
+  },
   server: {
     port: 3000,
     watch: {
