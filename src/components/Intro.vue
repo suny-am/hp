51c5ec201830dea9babb2a8c84849e508fdbb0ff
suny-am <visualarea.1@gmail.com>
2024-03-04 12:37:54 +0100

    Update intro #2

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -4,7 +4,7 @@
         <h1></h1>
         <h3></h3>
     </div>
-    <svg id="svg-container" width="550" height="550" ref="svgContainer">
+    <svg id="svg-container" :width="svgBoxWidth()" :height="svgBoxHeight()" ref="svgContainer">
         <g id="circles" :stroke="Data.colors.logo" fill="none">
 
         </g>
@@ -14,6 +14,13 @@
 <script setup lang="ts">
 import { onMounted, ref, Ref } from 'vue';
 import * as Data from "./data"
+
+const svgBoxWidth = () => {
+    return document.body.clientWidth
+},
+    svgBoxHeight = () => {
+        return document.body.clientHeight
+    }
 
 /**
  * 
@@ -43,10 +50,30 @@ const drawCircle = (centerX: number, centerY: number, radius: number): string =>
 onMounted(() => {
 
     if (titleContainer.value) {
-        console.log("HEY!")
         titleContainer.value.childNodes.item(0).textContent = Data.text.logoText1
         titleContainer.value.childNodes.item(1).textContent = Data.text.logoText2
         titleContainer.value.childNodes.item(2).textContent = Data.text.comingSoon
+
+        // TBD! Abstract away logic into globals/helpers?
+        setInterval(() => {
+            let textContent = titleContainer.value!.childNodes.item(2).textContent
+            switch (textContent) {
+                case "coming soon ...":
+                    titleContainer.value!.childNodes.item(2).textContent = "coming soon"
+                    break;
+                case "coming soon ..":
+                    titleContainer.value!.childNodes.item(2).textContent = "coming soon ..."
+                    break;
+                case "coming soon .":
+                    titleContainer.value!.childNodes.item(2).textContent = "coming soon .."
+                    break;
+                case "coming soon":
+                    titleContainer.value!.childNodes.item(2).textContent = "coming soon ."
+                    break;
+                default:
+                    break;
+            }
+        }, 500)
     }
 
     let circles = document.getElementById("circles")
@@ -54,7 +81,7 @@ onMounted(() => {
 
     let centerX = svgContainer.value.clientWidth / 2,
         centerY = svgContainer.value.clientHeight / 2,
-        refRadius = 60,
+        refRadius = svgBoxWidth() > svgBoxHeight() ? 150 : 150,
         solarRadius = 15,
         planetRadius = 5,
         solarPath = document.createElementNS(Data.links.xlmns, "path"),
@@ -89,7 +116,7 @@ onMounted(() => {
         planet.setAttribute("cx", `${radius}`)
         planet.setAttribute("cy", `${radius}`)
         planet.setAttribute("r", `${adjustedPlanetRadius}`)
-        planet.setAttribute("fill", Data.colors.foreground)
+        planet.setAttribute("fill", Data.colors.logo)
         planet.setAttribute("stroke", "none")
 
         let motion = document.createElementNS(Data.links.xlmns, "animateMotion")
@@ -114,34 +141,64 @@ onMounted(() => {
 
 <style lang="scss">
 @use "../styles/foundation/colors";
+@use "../styles/foundation/fonts";
 
-body {
+
+#app {
     display: flex;
     height: 100vh;
     width: 100vw;
-    justify-content: center;
-    align-items: center;
+    flex-flow: column;
     background: colors.$gradient;
+}
+
+
+#svg-container {
+    opacity: 0.85;
+    position: absolute;
+    overflow: visible;
+    top: 50%;
 }
 
 #title-container {
     display: flex;
     flex-flow: column;
-    align-self: center;
+    align-self: flex-start;
+    margin: 1rem;
+    position: absolute;
 
     h1,
-    h2,
-    h3 {
+    h2 {
         text-align: start;
         text-transform: uppercase;
         margin: 0;
         padding: 0;
+        color: colors.$logo;
+        line-height: 0.9em;
     }
 
-    h1,
-    h2 {
-        color: colors.$logo;
-        line-height: 1.4rem;
+    h3 {
+        margin-top: 0.25em;
+        font-weight: lighter;
+        font-family: fonts.$code ;
+    }
+
+    @media screen and (max-width: 300px) {
+        h1 {
+            font-size: 1.5em;
+        }
+        h2 {
+            font-size: 1.2em;
+        }
+        h3 {
+            font-size: small;
+        }
+    }
+
+    @media screen and (min-width: 750px) and (min-height: 1000px) {
+        h1 {
+            font-size: 3.5em;
+        }
     }
 }
 </style>
